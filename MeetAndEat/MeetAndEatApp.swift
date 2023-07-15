d9d90501813473d5867adca303c5888534fec874
jasmica140 <98131692+jasmica140@users.noreply.github.com>
2023-07-15 07:35:59 +0200

    Initial Commit

---
+++ MeetAndEat/MeetAndEatApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetAndEatApp.swift
+//  MeetAndEat
+//
+//  Created by Jasmine Micallef on 15/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct MeetAndEatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
